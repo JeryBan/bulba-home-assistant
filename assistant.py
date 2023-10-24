990d13dc44e156bc1f50d257ab88a30314c124cc
JeryBan <jerybananas@gmail.com>
2023-10-24 17:54:37 +0300

    take notes function

--- assistant.py
+++ assistant.py
@@ -1,10 +1,12 @@
 import speech_recog
+from take_notes import takeNote
 from playsound import playsound
 
 if __name__ == "__main__":
 
-	greetingList = ["bulba", "booba", "hey", "hello"]
-	goodbyeList = ["bye", "goodbye", "good", "night", "later"]
+	greetingKeys = ["bulba", "booba", "hey", "hello"]
+	goodbyeKeys = ["bye", "goodbye", "good", "night", "later"]
+	notesKeys = ["notes","note", "list", "supermarket"]
 
 	while True:
 		query = speech_recog.speechRec()
@@ -12,12 +14,19 @@ if __name__ == "__main__":
 
 		#---Commands----
 		for items in words:
-			if items in greetingList:
+			if items in greetingKeys:
 				playsound("./sounds/saaar.mp3")
 				break
 							
-			elif items in goodbyeList:
+			elif items in goodbyeKeys:
 				playsound("./sounds/bulbasaur-sad.mp3")
 				exit()
+
+			elif items in notesKeys:
+				takeNote()
+				print("done")
+
+
+
 
 			else: continue
---
+++ take_notes.py
@@ -0,0 +1,25 @@
+import os
+import speech_recog
+
+def takeNote():
+	returnKeys = ["thanks", "done", "finish", "ok"]
+
+	while True:
+		query = speech_recog.speechRec()
+		words = query.split(" ")
+
+		for items in words:
+			if items (not in returnKeys) and (items != "none"):
+				if os.path.isfile("./notes.txt"):
+					with open("./notes.txt", "a") as file:
+						notes = file.write(items + "\n")
+				else:
+					with open("./notes.txt", "w") as file:
+						notes = file.write(items + "\n")
+			elif items == "none":
+				continue
+			else:
+				return 0
+
+			
+				
